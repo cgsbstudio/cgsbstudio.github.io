356326517de97e9cc43a6871a9367fc8778f99f7
Stewart Bird <stewart.bird@ioioproduction.com>
2025-01-23 17:59:11 -0500

    casetify fix autoplay on mobile

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -30,4 +30,12 @@ function toggleMenu() {
         document.body.style.overflow = 'auto';
       }
     });
+
+    document.querySelectorAll('video').forEach(video => {
+      video.addEventListener('click', () => {
+          if (video.paused) {
+              video.play();
+          }
+      });
+  });
   }
